99be796beb65da1ec271ea37bd2b875041686efc
Vinícius Gibertoni Prado <92763040+Vini2405@users.noreply.github.com>
2022-11-15 19:13:54 -0300

    Update ac1_ldp_bd.sql

--- ac1_ldp_bd.sql
+++ ac1_ldp_bd.sql
@@ -3,10 +3,10 @@ CREATE DATABASE clientes;
 USE clientes;
 
 CREATE TABLE cliente (
-	CPF				VARCHAR(75)		primary key		not null,		
-	Nome			VARCHAR(100)	not null,
+	CPF			VARCHAR(75)		primary key		not null,		
+	Nome			VARCHAR(100)		not null,
 	Telefone		VARCHAR(45)		not null,
-    Cidade			VARCHAR(100)	not null,
-    UF				VARCHAR(30)		not null,
-    Email			VARCHAR(75)		not null
+	Cidade			VARCHAR(100)		not null,
+	UF			VARCHAR(30)		not null,
+	Email			VARCHAR(75)		not null
 )
